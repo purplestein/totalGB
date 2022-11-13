3f0b8e78b25acada8144b49c8d278418c6adad58
Evgeny <demidov_570@mail.ru>
2022-11-13 19:35:55 +0300

    added GetRand

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int[] array = GetRand(3,500,1999);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int[] GetRand(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue,maxValue);
+    }
+    return result;
+}
