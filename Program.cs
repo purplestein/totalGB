bca541d711a1a49eb2da7dc7da64ef013afb99a6
Evgeny <demidov_570@mail.ru>
2022-11-14 20:35:30 +0300

    new solution

--- Program.cs
+++ Program.cs
@@ -1,15 +1,7 @@
 ﻿using System;
 using static System.Console;
 
-Clear();
-int[] array = GetRand(3,500,1999);
-Console.WriteLine($"[{String.Join(", ", array)}]");
-int[] GetRand(int size, int minValue, int maxValue)
-{
-    int[] result = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        result[i] = new Random().Next(minValue,maxValue);
-    }
-    return result;
-}
+string[] M;
+WriteLine("Введите символы через пробел");
+string symbol = ReadLine();
+M = symbol.Split(' ');
