fd13d15392c5db0d026b15de4e317731392ec92b
Evgeny <demidov_570@mail.ru>
2022-11-14 20:40:17 +0300

    add foreach 3 symbols

--- Program.cs
+++ Program.cs
@@ -5,3 +5,15 @@ string[] M;
 WriteLine("Введите символы через пробел");
 string symbol = ReadLine();
 M = symbol.Split(' ');
+var result = new string[M .Length];
+var realSize = 0;
+    foreach (var value in M )
+    {
+        if (value.Length <= 3)
+        {
+            result[realSize] = value;
+            realSize++;
+        }
+    }
+WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+ReadKey(true);
